635c7462c3b17711ba5aedc136ed32b5d32d24ce
Naomi Dushay <ndushay@stanford.edu>
2019-03-08 11:20:02 -0800

    plumbing - package.json, .gitignore, .elsintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+// Copyright 2018 Stanford University see Apache2.txt for license
+module.exports = {
+  plugins: [
+    "import",
+    "jest",
+    "security"
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:node/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:security/recommended",
+    "plugin:jest/recommended"
+  ],
+  env: {
+    "jest": true,
+    "node": true
+  }
+}
