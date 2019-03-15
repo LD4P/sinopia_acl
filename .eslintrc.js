3b5d0ddbe260b85b0dfa77b5fbd94ac8766fce59
Naomi Dushay <ndushay@stanford.edu>
2019-03-15 15:18:32 -0700

    eslintrc.js: start tweaking those rules for stuff we can't figure out

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,23 @@ module.exports = {
   env: {
     "jest": true,
     "node": true
-  }
+  },
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: "module"
+  },
+  overrides: [
+    {
+      "files": ["src/*.js",
+                "__tests__/*.js"],
+      "rules": {
+        // See https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/no-unsupported-features/es-syntax.md
+        //   rule supposedly matches ECMA version with node
+        //   we get: "Import and export declarations are not supported yet"
+        "node/no-unsupported-features/es-syntax": "off",
+        // Avoiding: "warning  Found fs.readFileSync with non literal argument ..."
+        "security/detect-non-literal-fs-filename": "off"
+      }
+    }
+  ]
 }
